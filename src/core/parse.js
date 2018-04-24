65fd368c85515dcd9b9b6a7569b55749c8fdc676
Theo <theokobb@gmail.com>
2018-04-24 10:03:29 -0400

    Root is initialized as null. Check if root is null in iteration.

--- src/core/parse.js
+++ src/core/parse.js
@@ -8,15 +8,14 @@ const precedence = {
 /**
  * Transforms tokens into a syntax tree.
  * Algorithm:
- * Initialization:
- *   We store a reference to the root, and a reference to the leaf.
- *   There is at most one leaf, because all well-formed algebraic strings have complete binary trees.
- *   The first token is the root, and also the leaf.
  * For each token:
- *   If the token is a number, add it to the leaf. The root stays the leaf.
+ *   If the token is a number,
+ *     If the leaf exists, add it to the leaf. The root stays the leaf.
+ *     Otherwise, the root becomes the number.
  *   If the token is a binary operation:
+ *     If the root does not exist, throw.
  *     If the root is an operator, and has lower precedence than the operator,
- *       insert the operator between the root and its right child.
+ *       Insert the operator between the root and its right child.
  *       The new node becomes the leaf.
  *     Otherwise, the operator becomes the root.
  *       Attatch the previous root to the new root.
@@ -24,41 +23,48 @@ const precedence = {
  *   If the token is an open parenthesis:
  *     Create a subtree with all the tokens from the next token to the closing parenthesis.
  *     Remove the subexpression tokens from the tokens.
- *     Attatch the subtree to the leaf.
- *     The root becomes the leaf.
+ *     If the leaf does not exist, the subtree becomes the root.    
+ *     Otherwise,    
+ *       Attatch the subtree to the leaf.
+ *       The root becomes the leaf.
  * @param {*} tokens – lexer tokens
  */
 
 export default function parse(tokens){
-    if(tokens.length === 0){
-        throw new Error('empty expresison');
-    };
-    let root = new Node(tokens[0]);
+    let root = null;
     let leaf = root;
 
-    let index = 1;
+    let index = 0;
     while(index < tokens.length){
         let token = tokens[index];
         if(token instanceof Token._Number){
-            leaf.add(new Node(token));
+            if(leaf === null){
+                root = new Node(token);
+            } else {
+                console.log({leaf})
+                console.log({token})
+                leaf.add(new Node(token));
+            };
             leaf = root;
             index += 1;
             continue;
         }
         else if(token instanceof Token.BinaryOperation){
-            if((root.value instanceof Token.BinaryOperation) && token.precedence > root.value.precedence){
-                // high precedence: we keep the same root, and insert the operator below it.
-                const node = root.insertRight(new Node(token));
-                leaf = node;
-                index += 1;
-                continue;
+            if(root === null){
+                throw new Error('cannot apply operator to nothing');
             } else {
-                // low precedence: we replace the root and attach the current root
-                root = new Node(token, root);
-                leaf = root;
+                if((root.value instanceof Token.BinaryOperation) && token.precedence > root.value.precedence){
+                    // high precedence: we keep the same root, and insert the operator below it.
+                    const node = root.insertRight(new Node(token));
+                    leaf = node;
+                } else {
+                    // low precedence: we replace the root and attach the current root
+                    root = new Node(token, root);
+                    leaf = root;
+                };
                 index += 1;
                 continue;
-            };
+            }
         }
         else if(token instanceof Token.OpenParenthesis){
             /*
@@ -80,8 +86,12 @@ export default function parse(tokens){
             if(subExpressionTokens.length === 0){
                 throw new Error('empty expression');
             };
-            const subtree = parse(subExpressionTokens);
-            leaf.add(subtree);
+            const subtreeRoot = parse(subExpressionTokens);
+            if(root === null){
+                root = subtreeRoot;
+            } else {
+                leaf.add(subtreeRoot);
+            };
             leaf = root;
             index += subtreeLength + 2;
             continue;
