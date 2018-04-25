3a0c664cc1c0083da28154cfe27136b055d79b2c
Theo <theokobb@gmail.com>
2018-04-25 00:06:14 -0400

    Rewrite lex

--- src/core/lex/lex.js
+++ src/core/lex/lex.js
@@ -1,83 +1,79 @@
 import Token from './Token'
 
-function isWhitespace(char){
-    return /\s/.test(char);
-};
-
-function isDigit(char){
-    return /[\d\.]/.test(char);
-}
-
-function isBinaryOperation(char){
-    return /[+\-*\//]/.test(char);
-};
-
-function isParenthesis(char){
-    return /[\(\)]/.test(char);
-};
+const { parseFloat } = Number
 
 function tokenizeDigits(digits){
-    const rawNumber = digits.splice(0, digits.length).join('');
-    return new Token._Number(Number.parseFloat(rawNumber));
+    return new Token._Number(parseFloat(digits.join('')))
 };
 
-/** 
- * Takes an input string and returns a list of tokens.
- * Algorithm:
- * Initialization:
- *   We store a temporary stack of digits and the final list of tokens.
- * For every character of the input string:
- *   If the character is a digit, push it to the digit stack and check the next character.
- *   Otherwise,
- *     If there are digits on the digit stack, 
- *       combine them into a number and clear the digit stack.
- *       Add the number to the list of tokens.
- *     If the character is a binary operator,
- *       add it to the list of tokens and check the next character.
- *     If the character is a parenthesis,
- *       add it to the list of tokens and check the next character.
- *    
- * @param {String} expression – Input expression.
- * @returns {[ Tokens ]} tokens - List of tokens.
+/**
+ * Patterns.
+ * Keys are regular expressions.
+ * Values are functions of the current character, the tokens and the digits.
+ * They must return an object with keys { tokens, digits },
+ * coresponding to the new tokens and digits.
  */
-function tokenize(expression){
-    const digits = [];
-    const tokens = [];
-    for(let [index, char] of expression.split('').entries()){
-        if(isDigit(char)){
-            digits.push(char);
-            continue;
-        } else if(digits.length){
-            tokens.push(tokenizeDigits(digits));
-        };   
-        if(isWhitespace(char)){
-            continue;
-        }
-        else if(isBinaryOperation(char)){
-            tokens.push(new {
-                '+': Token.Addition,
-                '-': Token.Substraction,
-                '*': Token.Multiplication,
-                '/': Token.Division,
-            }[char]());
-            continue;
-        }
-        else if(isParenthesis(char)){
-            tokens.push(new {
-                '(': Token.OpenParenthesis,
-                ')': Token.CloseParenthesis
-            }[char]());
-            continue;
-        }
-        else {
-            throw new Error(`invalid character at position ${index}:'${char}'`);
-        };
-    };
-    if(digits.length){
-        tokens.push(tokenizeDigits(digits));
-    };
-    return tokens;
+
+function concatWithDigits(token, tokens, digits){
+    return digits.length === 0
+        ? tokens.concat(token)
+        : tokens.concat(
+            tokenizeDigits(digits),
+            token
+        );
+};
+
+const patterns = {
+    '\\s': (whitespace, tokens, digits) => ({
+        tokens: digits.length === 0
+            ? tokens
+            : tokens.concat(tokenizeDigits(digits)),
+        digits: []
+    }),
+    '[\\d\\.]': (digit, tokens, digits) => ({
+        tokens, digits: digits.concat(digit)
+    }),
+    '\\+': (binaryOperator, tokens, digits) => ({
+        tokens: concatWithDigits(new Token.Addition(), tokens, digits),
+        digits: []
+    }),
+    '\\-': (binaryOperator, tokens, digits) => ({
+        tokens: concatWithDigits(new Token.Substraction(), tokens, digits),
+        digits: []
+    }),
+    '\\/': (binaryOperator, tokens, digits) => ({
+        tokens: concatWithDigits(new Token.Division(), tokens, digits),
+        digits: []
+    }),
+    '\\*': (binaryOperator, tokens, digits) => ({
+        tokens: concatWithDigits(new Token.Multiplication(), tokens, digits),
+        digits: []
+    }),
+    '\\(': (parenthesis, tokens, digits) => ({
+        tokens: concatWithDigits(new Token.OpenParenthesis(), tokens, digits),
+        digits: []
+    }),
+    '\\)': (parenthesis, tokens, digits) => ({
+        tokens: concatWithDigits(new Token.CloseParenthesis(), tokens, digits),
+        digits: []
+    }),
+    '.': (char) => {
+        throw new Error(`invalid character:'${char}'`);
+    }
 }
+
+function tokenize(expression){
+    const { tokens, digits } = expression.split('').reduce(
+        ({ tokens, digits }, char, index) => {
+            return Object.entries(patterns)
+                .find(([ pattern, _ ]) => new RegExp(pattern).test(char))
+                [1](char, tokens, digits);
+        }
+    , { tokens: [], digits: [] });
+    return digits.length === 0
+        ? tokens    
+        : tokens.concat(tokenizeDigits(digits));
+};
 
 /**
  * Analyzes tokens and transforms them based on their relation to other tokens.
@@ -99,7 +95,7 @@ function transformTokens(_tokens){
                     return transformedTokens.concat(new Token._Number(-1 * next.value));
                 } else if (next instanceof Token.OpenParenthesis){
                     // multiply the whole expression by -1
-                    return transformedTokens.concat(new Token.Number, new Token.Multiplication());
+                    return transformedTokens.concat(new Token._Number, new Token.Multiplication());
                 }
             }
         }
