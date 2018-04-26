6ab225585604a8c13800dff4e91431b0515e3891
Theo <theokobb@gmail.com>
2018-04-26 18:08:05 -0400

    Improve gcd algorithm

--- src/util/gcd.js
+++ src/util/gcd.js
@@ -1,15 +1,13 @@
-const { min, abs } = Math;
+const { min, max, abs } = Math;
+
+function _gcd(a, b){
+    if(b === 0) return a;
+    return _gcd(b, a % b);
+}
 
 export default function gcd(a, b){
     if(a === 0 || b === 0){
         throw new Error('Cannot find divisors of 0');
     };
-    let x = min(abs(a), abs(b));
-    while(x >= 1){
-        if(a % x === 0 && b % x === 0){
-            return x;
-        };
-        x -= 1;
-    };
-    return x;
+    return _gcd(abs(a), abs(b));
 };
