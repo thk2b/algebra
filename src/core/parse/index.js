2b410a8a8d47f3acd27535988e2fd5ba9526e77a
Theo <theokobb@gmail.com>
2018-04-27 11:32:09 -0400

    Add semis

--- src/core/parse/index.js
+++ src/core/parse/index.js
@@ -1,3 +1,3 @@
-export { default } from './parse'
-export { default as Node } from './Node'
-export { default as ParseError } from './ParseError'
+export { default } from './parse';
+export { default as Node } from './Node';
+export { default as ParseError } from './ParseError';
